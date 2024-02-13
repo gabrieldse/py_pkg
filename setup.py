d5aea4bca53f79fde0af3fd8d5b6f356b1c507db
Gabriel WSL <gabriel.oliveira.dse@gmail.com>
2024-02-13 10:33:06 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+# launch inports
+import subprocess, os, platform
+from glob import glob
+
+package_name = 'py_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share',package_name,'launch'),
+        glob('launch/*.py'))
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='ubuntu@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+    	'console_scripts': [
+	"py_node = py_pkg.node1:main",
+    "robot = py_pkg.robot:main",
+    "sub = py_pkg.subs:main",
+    "control_turtle = py_pkg.control_turtle:main"
+    ],
+	},
+)
